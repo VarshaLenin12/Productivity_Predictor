df784ce593f15296fc72ed1af542b0ba3f5e71b0
VarshaLenin12 <120273904+VarshaLenin12@users.noreply.github.com>
2024-07-02 20:46:37 +0530

    Update settings.py

--- productivity_predictor/settings.py
+++ productivity_predictor/settings.py
@@ -123,6 +123,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # Adjust as per your project structure
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
